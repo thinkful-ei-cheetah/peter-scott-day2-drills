e69b9218ffbc67d88cab401b913b29b04677e701
Peter Pae <paendabear@gmail.com>
2019-03-26 14:11:53 -0400

    add drillfunct.

---
+++ higher-order.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line strict
+function repeat(fn,n){
+    for (let i=0; i < n; i++) {
+        fn();
+    }
+}
+
+function hello () {
+    console.log("hello world");
+}
+
+function goodbye() {
+    console.log("Goodbye world");
+}
+
+repeat(hello,5);
+repeat(goodbye, 5);
+
