e659fca7dc73fd8375c07c9735afd55e5beb4256
Peter Pae <paendabear@gmail.com>
2019-03-26 12:28:46 -0400

    finish array js part one

---
+++ arrayloops.js
@@ -0,0 +1,42 @@
+/* eslint-disable indent */
+/* eslint-disable no-empty */
+/* eslint-disable strict */
+function max(num) {
+    if (num.length === 0){
+        return undefined;
+    }
+    else{
+    let max = num[0];
+    let i=0;
+    while (i < num.length) {
+        if (max < num[i]){
+            max = num[i];
+        }
+        i++;
+    }
+    return max;
+}
+}
+
+let numb = [1,2,3,4,5,6];
+
+console.log(max(numb));
+
+function min(num) {
+    if (num.length === 0){
+        return undefined;
+    }
+    else {
+    let min = num[0];
+    let i=0;
+    while (i<num.length) {
+        if (min > num[i]){
+            min = num[i];
+        }
+        i++;
+    }
+    return min;
+}
+}
+
+console.log(min(numb));
